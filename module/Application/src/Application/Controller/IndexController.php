af455009cd589398215d8c9f2e834fca2bdc1aa1
Kyle Andrews <kandrew1@yourdirectlink.com>
2018-09-22 00:58:34 -0400

    Working doctrine codebase along with working registered users call.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,13 +9,53 @@
 
 namespace Application\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\JsonModel;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
+class IndexController extends AbstractActionController {
+
+    /**
+     * @var EntityManager $em
+     */
+    protected $em;
+
+    private function getEntityManager(){
+        if(null === $this->em){
+            $this->em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        }
+        return $this->em;
+    }
+
+    public function indexAction() {
+        $entityName = 'Application\Entity\Registration';
+
+        /**
+         * @var \Application\Entity\Registration $entityResult
+         */
+        $entityResult = $this->getEntityManager()->getRepository($entityName)->findAll();
+
+        var_dump($entityResult);die('end');
+
         return new ViewModel();
+    }
+
+    public function getAdminReportAction() {
+        $entityName = 'Application\Entity\Registration';
+
+        /**
+         * @var \Application\Entity\Registration $entityResult
+         */
+        $entityResult = $this->getEntityManager()->getRepository($entityName)->findAll();
+
+        /** @var \Application\Entity\Registration[] $registeredUsers */
+        $registeredUsers = [];
+        /** @var \Application\Entity\Registration $result */
+        foreach($entityResult as $result) {
+            $registeredUsers[] = $result->getArrayCopy();
+        }
+
+        return new JsonModel(['registeredUsers' => $registeredUsers]);
     }
 }
